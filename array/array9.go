b49f47a5672c86398b95097711ff6e62224d3279
Q00 <jqyu.lee@gmail.com>
2020-08-18 06:31:30 +0900

    Two Sum

---
+++ array/array9.go
@@ -0,0 +1,34 @@
+package array
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+// TwoSum add two propery to make target
+func TwoSum(nums []int, target int) []int {
+	check := map[int][]int{}
+
+	for i, v := range nums {
+		check[v] = append(check[v], i)
+
+		if len(check[target-v]) >= 1 {
+			// same value has two property in array
+			if v == target-v && len(check[v]) > 1 {
+				return check[v]
+			} else if v != target-v {
+				return []int{check[target-v][0], check[v][0]}
+			}
+		}
+	}
+
+	return nums
+}
