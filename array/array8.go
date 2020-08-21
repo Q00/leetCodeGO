4405c19760215170647e6940432eb7a4d01cbe9a
Q00 <jqyu.lee@gmail.com>
2020-08-22 06:32:47 +0900

    find not 0 value and overwrite

--- array/array8.go
+++ array/array8.go
@@ -1,9 +1,5 @@
 package array
 
-import (
-	"sort"
-)
-
 /*
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
@@ -19,22 +15,36 @@ Minimize the total number of operations.
 
 // MoveZeroes move zero backward
 func MoveZeroes(nums []int) {
-	check := false
+	// check := false
+	// for _, value := range nums {
+	// 	if value == 0 {
+	// 		check = true
+	// 	}
+	// }
+
+	// if check {
+	// 	// not guaranted argument of list
+	// 	// sort.Slice(nums, func(_, j int) bool {
+	// 	// return nums[j] == 0
+	// 	// })
+
+	// 	// use SliceStable
+	// 	sort.SliceStable(nums, func(_, j int) bool {
+	// 		return nums[j] == 0
+	// 	})
+	// }
+
+	writeIndex := 0
 	for _, value := range nums {
-		if value == 0 {
-			check = true
+		if value != 0 {
+			nums[writeIndex] = value
+
+			writeIndex++
 		}
 	}
 
-	if check {
-		// not guaranted argument of list
-		// sort.Slice(nums, func(_, j int) bool {
-		// return nums[j] == 0
-		// })
-
-		// use SliceStable
-		sort.SliceStable(nums, func(_, j int) bool {
-			return nums[j] == 0
-		})
+	for i := writeIndex; i < len(nums); i++ {
+		nums[i] = 0
 	}
+
 }
